d2b5bbc99915280ecb936f8b08fb7d070b308ed2
Vagner Clementino <vagner.clementino@gmail.com>
2021-03-17 22:28:48 -0300

    @vagnerclementino - Adds first version from asaideira store (#2)
    * @vagnerclementino - Adds first version from asaideira store
    
    * @vagnerclementino - Refactors in the code
    
    * @vagnerclementino - Removes from pom.xml no used lombok version
    
    Co-authored-by: Vagner Clementino <vagner.clementino@hotmart.com>

---
+++ src/main/java/me/clementino/domain/tipos/AcaiNoCopo.java
@@ -0,0 +1,26 @@
+package me.clementino.domain.tipos;
+
+import me.clementino.domain.base.Acai;
+import me.clementino.domain.base.Tamanho;
+
+public class AcaiNoCopo extends Acai {
+
+    public AcaiNoCopo(Tamanho tamanho) {
+        super.setTamanho(tamanho);
+        super.setDescricao("Acaí no copo");
+    }
+
+    @Override
+    public double cost() {
+        switch (super.getTamanho()) {
+            case GRANDE:
+                return 9.99;
+            case MEDIO:
+                return 5.99;
+            case PEQUENO:
+                return 2.99;
+            default:
+                return .0;
+        }
+    }
+}
